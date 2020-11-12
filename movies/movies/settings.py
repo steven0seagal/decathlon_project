fdb740ee13f38ec09bfa3922fffd717ea77f4973
Bartek <bartosz.piotr.baranowski@gmail.com>
2020-11-12 23:09:13 +0100

    static setting

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -121,7 +121,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '/static')
+
+]
+
+# Media folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 APPEND_SLASH=True
 
